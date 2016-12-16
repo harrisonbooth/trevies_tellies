8707921be28414c105ebc2c718275b4bc940f7d7
Harrison Booth <harrisonbooth@ymail.com>
2016-12-16 17:59:58 +0000

    create new manufacturer button and function

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -4,10 +4,19 @@ require_relative( '../models/manufacturer.rb')
 require_relative( '../models/television.rb')
 
 # # get new manufacturer form
-
+get( '/manufacturers/new') do
+  erb( :"/manufacturer/new" )
+end
 
 
 # # post create manufacturer
+post( '/manufacturers' ) do
+  @manufacturer = Manufacturer.new( params )
+  @manufacturer.save()
+  redirect to( '/televisions' )
+end
 
 
 # # post delete manufacturers
+
+
