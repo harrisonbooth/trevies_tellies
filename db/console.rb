07afce55c78bfec5d9ebaecc429f171137890a0d
Harrison Booth <harrisonbooth@ymail.com>
2016-12-17 14:09:29 +0000

    add more television seeds to console.rb

--- db/console.rb
+++ db/console.rb
@@ -4,32 +4,76 @@ require_relative( '../models/television.rb' )
 # require_relative( 'models/manufacturer.rb' )
 
 television1 = Television.new( {
-      'model_no' => 'UE55JS8000',
-      'manufacturer_id' => 1,
-      'stock' => 9,
-      'cost_price' => 1
-    } )
+  'model_no' => 'UE55JS8000',
+  'manufacturer_id' => 1,
+  'stock' => 9,
+  'cost_price' => 1300
+  } )
 
 television2 = Television.new( {
-      'model_no' => 'KDL-55XD850',
-      'manufacturer_id' => 2,
-      'stock' => 4,
-      'cost_price' => 10
-    } )
+  'model_no' => 'KDL-55XD850',
+  'manufacturer_id' => 2,
+  'stock' => 4,
+  'cost_price' => 1000
+  } )
 
 television3 = Television.new( {
-      'model_no' => 'KDL-49WD750',
-      'manufacturer_id' => 2,
-      'stock' => 2,
-      'cost_price' => 100
-    } )
+  'model_no' => 'KDL-49WD750',
+  'manufacturer_id' => 2,
+  'stock' => 2,
+  'cost_price' => 550
+  } )
+
+television4 = Television.new( {
+  'model_no' => 'KDL-43WD750',
+  'manufacturer_id' => 2,
+  'stock' => 6,
+  'cost_price' => 400
+  } )
+
+television5 = Television.new( {
+  'model_no' => 'TX-55DX800',
+  'manufacturer_id' => 3,
+  'stock' => 23,
+  'cost_price' => 1000
+  } )
+
+television6 = Television.new( {
+  'model_no' => 'TX-49DX700',
+  'manufacturer_id' => 3,
+  'stock' => 4,
+  'cost_price' => 850
+  } )
+
+television7 = Television.new( {
+  'model_no' => 'UE-49JU7500',
+  'manufacturer_id' => 1,
+  'stock' => 8,
+  'cost_price' => 1000
+  } )
+
+television8 = Television.new( {
+  'model_no' => 'KDL-55XD930',
+  'manufacturer_id' => 2,
+  'stock' => 3,
+  'cost_price' => 850
+  } )
+
+television9 = Television.new( {
+  'model_no' => 'UE-49JU6100',
+  'manufacturer_id' => 1,
+  'stock' => 4,
+  'cost_price' => 650
+  } )
 
 television1.save()
 television2.save()
 television3.save()
-
-2.times { television1.save() }
-2.times { television2.save() }
-2.times { television3.save() }
+television4.save()
+television5.save()
+television6.save()
+television7.save()
+television8.save()
+television9.save()
 
 binding.pry()
