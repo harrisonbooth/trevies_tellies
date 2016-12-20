fd3d6d6ea3ab9f3f216f574c6209837d27d6fa48
Harrison Booth <harrisonbooth@ymail.com>
2016-12-20 13:43:27 +0000

    Add comments to shared functions file

--- controllers/shared_functions.rb
+++ controllers/shared_functions.rb
@@ -1,3 +1,5 @@
+# Defines what to sort on when sorting tvs to go into indexes and drop downs
+# Save a lot of code looking very ugly in controllers and indexes... :)
 def tv_sort_criteria( tv1, tv2 )
   [ tv1.manufacturer.name, tv2.model_no, tv2.retail_price ] <=> [ tv2.manufacturer.name, tv1.model_no, tv2.retail_price ]
 end
