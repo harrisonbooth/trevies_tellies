cc14933b91f25f30ec67927933b5514debb521d5
Harrison Booth <harrisonboothh@gmail.com>
2019-06-10 16:58:13 +0100

    set up some heroku stuff

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,13 @@ class SqlRunner
 
   def self.run( sql )
     begin
-      db = PG.connect( { dbname: 'trevies_tellies', host: 'localhost' } )
+      db = PG.connect({
+        dbname: 'dgl2j6ld49vjo',
+        host: 'ec2-54-247-178-166.eu-west-1.compute.amazonaws.com',
+        port: 5432, user: 'rywkxypanrogce',
+        password: 'a4118df094cb0fe7a61f581f7605229980ca202dd4b8f5934f68cb55c53d5339'
+      })
+
       result = db.exec( sql )
     ensure
       db.close()
